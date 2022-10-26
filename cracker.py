7900c8b00ef219e5adb0e935029092b6951bbcf1
MrGeek117 <116690375+MrGeek117@users.noreply.github.com>
2022-10-26 04:55:45 +0200

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,5 +1,5 @@
 import requests
-from update_RB.py import update_RB_file, set_boundary
+from update_RB import update_RB_file, set_boundary
 
 ####################################
 ### ----- GET SESSION DATA ----- ###
