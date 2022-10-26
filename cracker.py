7b3fc71bcdc8e71999fb724eeff5f9d59ab0010a
0xJ0HN7170R <116690424+0xJ0HN7170R@users.noreply.github.com>
2022-10-26 01:51:00 +0000

    Add files via upload

--- cracker.py
+++ cracker.py
@@ -1,27 +1,22 @@
 import requests
 
-headers = {
-"Host": "www.seed-server.com",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
-"Accept": "application/json, text/javascript, */*; q=0.01",
-"Accept-Language": "en-US,en;q=0.5",
-"Accept-Encoding": "gzip, deflate",
-"Referer": "http://www.seed-server.com/login",
-"X-Elgg-Ajax-API": "2",
-"X-Requested-With": "XMLHttpRequest",
-"Content-Type": "multipart/form-data; boundary=---------------------------23149188723087745730519093566",
-#"Content-Length": "558",
-#"Origin": "http://www.seed-server.com",
-#"DNT": "1",
-#"Connection": "keep-alive",
-"Cookie": "Elgg=ithv8nt4af8rvn3naaagaaug7k"
-}
 url = "http://www.seed-server.com/action/login"
 f = open("/home/c4ll3/elgg-testing/login_cracker/request_body", "r")
-payload = f.read()
-test = requests.post(url, headers = headers, data = payload)
-print(test)
-print(test.request.headers)
-print(test.request.url)
-print(test.request.body)
-print(test.content)
+data = f.read()
+s = requests.Session()
+r = s.get("http://www.seed-server.com/refresh_token")
+cookies = r.headers["Set-Cookie"].split(";")
+cookie = cookies[0]
+x = r.text.split("\"")
+ts = x[4]
+ts = ts[1:len(ts)-1]
+token = x[7]
+headers = {
+"X-Elgg-Ajax-API": "2",
+"X-Requested-With": "XMLHttpRequest",
+"Content-Type": "multipart/form-data; boundary=---------------------------335837917637707572332315839054",
+"Cookie": f"{cookie}"
+}
+#l = s.post(url, headers = headers, data = data)
+#print(l)
+#print(l.text)
