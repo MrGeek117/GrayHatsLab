6d3e82e76f4b568797092437c0358d543b81879e
0xJ0HN7170R <116690424+0xJ0HN7170R@users.noreply.github.com>
2022-10-26 02:18:37 +0000

    Add files via upload

--- cracker.py
+++ cracker.py
@@ -1,8 +1,11 @@
 import requests
 
-url = "http://www.seed-server.com/action/login"
-f = open("/home/c4ll3/elgg-testing/login_cracker/request_body", "r")
-data = f.read()
+#f = open("/home/c4ll3/elgg-testing/login_cracker/request_body", "r")
+#data = f.read()
+
+####################################
+### ----- GET SESSION DATA ----- ###
+####################################
 s = requests.Session()
 r = s.get("http://www.seed-server.com/refresh_token")
 cookies = r.headers["Set-Cookie"].split(";")
@@ -11,12 +14,25 @@ x = r.text.split("\"")
 ts = x[4]
 ts = ts[1:len(ts)-1]
 token = x[7]
+### --------------------------- ###
+# ------------------------------- #
+###################################
+### ------ BUILD REQUEST ------ ###
+###################################
+url = "http://www.seed-server.com/action/login"
 headers = {
 "X-Elgg-Ajax-API": "2",
 "X-Requested-With": "XMLHttpRequest",
 "Content-Type": "multipart/form-data; boundary=---------------------------335837917637707572332315839054",
 "Cookie": f"{cookie}"
 }
+### --------------------------- ###
+# ------------------------------- #
+###################################
+### ------- POST REQUEST ------ ###
+###################################
 #l = s.post(url, headers = headers, data = data)
 #print(l)
 #print(l.text)
+### --------------------------- ###
+# ------------------------------- #
