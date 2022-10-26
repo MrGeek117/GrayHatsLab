37b82e35cfb4d5ea22176c2ee578b70b23766d3b
0xJ0HN7170R <116690424+0xJ0HN7170R@users.noreply.github.com>
2022-10-26 03:11:55 +0000

    Add files via upload

--- update_RB.py
+++ update_RB.py
@@ -4,7 +4,6 @@ import random
 
 ## Boundaries can change but total lenght stays the same
 
-
 class file_RB:
     def __init__(self):
         self.token_line = 'Content-Disposition: form-data; name="__elgg_token"'
@@ -20,9 +19,9 @@ def randN(N):
 
 
 def set_boundary():
-    s_newB = str(randN(random.randrange(10,40)))
+    s_newB = str(randN(random.randrange(30, 31)))
     global boundary 
-    boundary = (58-len(s_newB))*"-" + s_newB
+    boundary = (59-len(s_newB))*"-" + s_newB
     return boundary
 
 def Get_RB_string(user, passw, token, timestamp):
@@ -40,8 +39,3 @@ def update_RB_file(user, passw, token, timestamp):
         #except:
             #print("Insert the right types in the right order: User(str), Passw(str), token(str),timestamp(int/str)")
     return "./temp_body"
-
-
-print(set_boundary())
-print(get_boundary())
-update_RB_file("Tony", "1", "1", "1")
