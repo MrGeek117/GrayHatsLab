72eceb0464332ea0c79e7b97fa2079dc46920278
MrGeek117 <116690375+MrGeek117@users.noreply.github.com>
2022-10-27 00:33:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-from update_RB import update_RB_file, set_boundary
 
-####################################
-### ----- GET SESSION DATA ----- ###
-####################################
-s = requests.Session()
-s.headers.update({
-"Host": "www.seed-server.com",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-"Accept-Language": "en-US,en;q=0.5",
-"Accept-Encoding": "gzip, deflate",
-"DNT": "1",
-"Connection": "keep-alive",
-"Upgrade-Insecure-Requests": "1"
-})
-r = s.get("http://www.seed-server.com/login")
-soup = BeautifulSoup(r.text, 'html.parser')
-# print(soup.body.script)
-# print(soup.body.script.contents[0][643:653])
-# print(soup.body.script.contents[0][670:693])
-ts = soup.body.script.contents[0][643:653]
-token = soup.body.script.contents[0][670:692]
-### --------------------------- ###
-# ------------------------------- #
-###################################
+
+### ----- GET SESSION DATA OBJ ----- ###
+class Session:
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update({
+        "Host": "www.seed-server.com",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+        })
+        self.request = self.session.get("http://www.seed-server.com/login")
+        self.soup = BeautifulSoup(self.request.text, 'html.parser')
+        self.ts = self.soup.body.script.contents[0][643:653]
+        self.token = self.soup.body.script.contents[0][670:692]
+        
+
+def PostRequest(SessionData, password):
+    #SessionData = Session() ##Remove it and construct the Session Data outside
 ### ------ BUILD REQUEST ------ ###
-###################################
-url = "http://www.seed-server.com/action/login"
-### --------------------------- ###
-# ------------------------------- #
-###################################
+    url = "http://www.seed-server.com/action/login"
+
 ### ------- POST REQUEST ------ ###
-###################################
-l = s.get(url, params = {
-    "__elgg_token": token,
-    "__elgg_ts": ts,
-    "username": "alice",
-    "password": "seedalic"
-})
-nsoup = BeautifulSoup(l.text, 'html.parser')
-print(nsoup.body.find('div', 'elgg-body').get_text())
+
+    l = SessionData.session.get(url, params = {
+        "__elgg_token": SessionData.token,
+        "__elgg_ts": SessionData.ts,
+        "username": "alice",
+        "password": "seedalice"
+        })
+    nsoup = BeautifulSoup(l.text, 'html.parser')
+    print(nsoup.body.find('div', 'elgg-body').get_text())
+
+
 ### --------------------------- ###
-# ------------------------------- #
+
