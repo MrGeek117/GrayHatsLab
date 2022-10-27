d9cf978b8f99d455bc875c7faaefd4bffc7218f5
0xJ0HN7170R <116690424+0xJ0HN7170R@users.noreply.github.com>
2022-10-27 13:40:51 +0000

    Removed `headers` and unneccesary `print()`s.

--- SendPostForm.py
+++ SendPostForm.py
@@ -6,16 +6,6 @@ from bs4 import BeautifulSoup
 class Session:
     def __init__(self):
         self.session = requests.Session()
-        self.session.headers.update({
-        "Host": "www.seed-server.com",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "DNT": "1",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1"
-        })
         self.request = self.session.get("http://www.seed-server.com/login")
         self.soup = BeautifulSoup(self.request.text, 'html.parser')
         self.ts = self.soup.body.script.contents[0][643:653]
@@ -35,12 +25,9 @@ def PostRequest(SessionData, password):
         "username": "alice",
         "password": password
         })
-    print(SessionData.ts)
-    print(SessionData.token)
     
     nsoup = BeautifulSoup(l.text, 'html.parser')
     log_in_mess = nsoup.body.find('div', 'elgg-body').get_text()
-    print(log_in_mess)
     if log_in_mess == "You have been logged in.":
         print("Successfull")
         return True
@@ -51,5 +38,3 @@ def PostRequest(SessionData, password):
         print("WTF. This is unexpected...")
 
 ### --------------------------- ###
-
-
